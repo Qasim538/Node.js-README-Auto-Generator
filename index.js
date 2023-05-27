e2049d867f8e7eaab5a83c4c6b32d6c17d2bcec9
Qasim <mq12113@gmail.com>
2023-05-27 14:54:14 +0100

    Added questions and run query function

--- index.js
+++ index.js
@@ -1,3 +1,72 @@
 const inquirer = require('inquirer')
 
-console.log('readme gen running')
+
+// App Questions
+
+const questions = [
+    
+    {
+        type: 'input',
+        name: 'title',
+        message: 'project title',
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'project description',
+    },
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'project usage',
+    },
+
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'contribution info',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'for question(email)',
+    },
+
+    {
+        type: 'input',
+        name: 'github',
+        message: 'for question(github)',
+    },
+
+    {
+        type: 'list',
+        name: 'license',
+        message: 'License',
+        choices:['MIT', 'ISC', 'GNULPv3'],
+        filter(val) {
+            return val.tolowerCase();
+        }
+    },
+
+    
+]
+
+// run query function
+
+function runQuery() {
+    return inquirer.prompt(questions)
+    .then((answers)=> {
+        console.log(answers)
+        return answers
+    })
+
+    .catch((error) => { 
+        console.log(error)
+    })
+}
+
+runQuery()
+
